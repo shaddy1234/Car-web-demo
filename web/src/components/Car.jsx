22d249d7a2dd3fe6f2e725af42534ce8e6f9e62e
Shadrack Kimaau <musyokishadrach1@gmail.com>
2024-02-03 07:36:32 +0300

    Update the component details

--- web/src/components/Car.jsx
+++ web/src/components/Car.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
+import './Car.css'
 
 function Car() {
-  return (
-    <div className=''>
-        <div>
-            <img src="../images/car-1.jpg" alt="Photo-1" />
+    return (
+        <div className='one'>
+            <img src="../images/car-1.jpg" alt="Photo-1" className='car-photo' />
+            <div className='car-details'>
+                <span>NISSAN NOTE </span>
+                <span>2021</span>
+            
+            <p>Nissan Note comes with a wide range of equipments with some models adopting a 4 Wheel drive system like the one listed above. You still get a large number of safety features making the Nissan Note a very competitive mini Multipurpose van. The safety features include Anti-collision brake assist, Traction control, and lane keep assist. The drivetrain gets you sorted in any kind of terrain especially while offroading.</p>
+            <br />
+            <hr />
+            <span>KES 1,200,000</span>
+            <span>AVAILABLE</span>
+            </div>
         </div>
-        <div className='car-details'>
-
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Car
