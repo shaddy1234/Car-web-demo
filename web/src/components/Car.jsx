c54db9fd6273aeb612b3611eba608a053ed1de3c
Shadrack Kimaau <musyokishadrach1@gmail.com>
2024-02-03 06:33:41 +0300

    Components folder to store components

---
+++ web/src/components/Car.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Car() {
+  return (
+    <div className=''>
+        <div>
+            <img src="../images/car-1.jpg" alt="Photo-1" />
+        </div>
+        <div className='car-details'>
+
+        </div>
+    </div>
+  )
+}
+
+export default Car
