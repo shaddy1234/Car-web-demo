5ff6f007968e2fdabca4cbafbc6161f0c9f2e59d
Shadrack Kimaau <musyokishadrach1@gmail.com>
2024-02-05 19:30:43 +0300

    Remove data

--- web/src/components/data.jsx
+++ web/src/components/data.jsx
@@ -1,14 +1,14 @@
-export default [
-    {
-        id:1,
-        name:'Nissan NOTE 2021',
-        YOM:2021,
-        ModeOfTransmission:Automatic,
-        CC:2700,
-        country:'KenyaN Used',
-        price:'1,200,000',
-        availability:'AVAILABLE',
-        stock:'OUR STOCK',
-    },
+// export default [
+//     {
+//         id:1,
+//         name:'Nissan NOTE 2021',
+//         YOM:2021,
+//         ModeOfTransmission:Automatic,
+//         CC:2700,
+//         country:'Kenyan Used',
+//         price:'1,200,000',
+//         availability:'AVAILABLE',
+//         stock:'OUR STOCK',
+//     },
 
-]
+// ]
