8b620c3f6a938fb43621402a016e42877ceb092f
Shadrack Kimaau <musyokishadrach1@gmail.com>
2024-02-05 12:46:58 +0300

    Store data as object

--- web/src/components/data.jsx
+++ web/src/components/data.jsx
@@ -1,7 +1,14 @@
 export default [
     {
         id:1,
-        name:Mazda
+        name:'Nissan NOTE 2021',
+        YOM:2021,
+        ModeOfTransmission:Automatic,
+        CC:2700,
+        country:'KenyaN Used',
+        price:'1,200,000',
+        availability:'AVAILABLE',
+        stock:'OUR STOCK',
     },
 
 ]
