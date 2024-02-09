6d917e42b7934722b77b4cc0695d13a2dd9f6eaa
Shadrack Kimaau <musyokishadrach1@gmail.com>
2024-02-09 13:03:03 +0300

    Nav-changes

--- web/src/components/Navbar.jsx
+++ web/src/components/Navbar.jsx
@@ -1,17 +1,25 @@
 import React from 'react'
+import './Car.css'
 
 function Navbar() {
   return (
     <div>
-      <img src="../images/logo/png" alt="logo" />
-      <div>
-        <ul>
-            <li><a href="#">Vehicles In our Yard</a></li>
-            <li><a href="#">Vehicles Outside our Stock</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Contact</a></li>
-        </ul>
-      </div>
+        <header className="header">
+            <img 
+                src="../images/logo.png" 
+                className="header--image"
+            />
+            <h2 className="header--title">Kim's Web</h2>
+
+            <div >
+                <ul className='navbar'>
+                    <li><a href="#">Vehicles In our Yard</a></li>
+                    <li><a href="#">Vehicles Outside our Stock</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
+        </header>
     </div>
   )
 }
