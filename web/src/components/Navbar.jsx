31912de1d3f6ee42291a30ce0435f38c66e0a231
Shadrack Kimaau <musyokishadrach1@gmail.com>
2024-02-09 12:26:16 +0300

    Create Navbar component

---
+++ web/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+      <img src="../images/logo/png" alt="logo" />
+      <div>
+        <ul>
+            <li><a href="#">Vehicles In our Yard</a></li>
+            <li><a href="#">Vehicles Outside our Stock</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
